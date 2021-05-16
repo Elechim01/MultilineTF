07f7483feabeca941227817fc0af0ab8f3e847c3
Elechim01 <mikymanny015@gmail.com>
2021-05-16 17:15:03 +0200

    Initial Commit

---
+++ MultilineTF/MultilineTFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultilineTFApp.swift
+//  MultilineTF
+//
+//  Created by Michele Manniello on 16/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MultilineTFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
